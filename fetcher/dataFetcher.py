a0250a6c9d1cdb3414a8c53131c8c3d0634acba3
hjalmar <hjalmar.thunberg@gmail.com>
2021-11-21 16:28:37 +0100

    Added more dynamic support by including argsparser

--- fetcher/dataFetcher.py
+++ fetcher/dataFetcher.py
@@ -1,11 +1,16 @@
 # import libraries
 import pandas_datareader as pdr
 import sqlite3
-import os
 from datetime import datetime, timedelta 
+import argparse
+import os
 
- # fetcher attributes
-stocks = ['GME', 'RBLX', 'NVDA', 'TSLA', 'COIN', 'FB', 'BYND', 'AAPL', 'AMC', 'MSFT', 'SHOP', 'ACB', 'TLRY', 'NFLX', 'GOOG', 'AMZN', 'DIS', 'BYND', 'ZM', 'PTON']
+# parser attributes
+parser = argparse.ArgumentParser()
+parser.add_argument("--list", nargs="+", default=['GME', 'RBLX', 'NVDA', 'TSLA', 'COIN', 'FB', 'BYND', 'AAPL', 'AMC', 'MSFT', 'SHOP', 'ACB', 'TLRY', 'NFLX', 'GOOG', 'AMZN', 'DIS', 'BYND', 'ZM', 'PTON'])
+stocks = parser.parse_args()
+
+ # pandas_datareader attributes
 source = 'yahoo'
 today = datetime.now() # todays date (YYYY-MM-DD HH:MM:SS)
 years = 10             # years back to start from
@@ -16,8 +21,7 @@ conn = sqlite3.connect(os.path.realpath('../data/stock.db'))
 c = conn.cursor()
 
 # fetch data for each stock
-for x in stocks:
+for x in stocks.list:
     df = pdr.DataReader(x, data_source=source, start=startYear, end=today)
     df.to_sql(x, conn, if_exists='replace')
     print(df)
-
