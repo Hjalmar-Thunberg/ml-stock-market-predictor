a54666b05f8d82a2c7f7e2af3051ea3ebdf461af
KardoMarof <gusmaroka@student.gu.se>
2021-12-05 02:29:14 +0100

    dockerize ML component and frontend

---
+++ frontend/reactInterface/docker.sh
@@ -0,0 +1,10 @@
+docker build -t frontend .
+
+docker run --name frontend \
+    -itd \
+    --rm \
+    -v ${PWD}:/app \
+    -v /app/node_modules \
+    -p 3001:3000 \
+    -e CHOKIDAR_USEPOLLING=true \
+    frontend
---
+++ initiate.sh
@@ -0,0 +1,6 @@
+cd frontend/reactInterface
+bash docker.sh
+cd ../../backend/api/
+bash docker.sh
+cd ../../jupyter\ models/
+bash docker.sh
