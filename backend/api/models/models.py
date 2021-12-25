6c80d754bf085fe94ad5532d9b62c39d7ab9babb
Christian O'Neill <oneill.christian97@gmail.com>
2021-12-25 21:40:33 +0100

    Add nullable fields to allow admin to add stocks

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -6,14 +6,14 @@ class PredictionModel(models.Model):
 
     version = models.IntegerField(default=1, validators=[MinValueValidator(1)])
     for_stock = models.CharField(max_length=200)
-    num_nodes = models.IntegerField()
-    acc_50 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_60 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_70 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_80 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_90 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_95 = models.DecimalField(max_digits=4, decimal_places=2)
-    acc_99 = models.DecimalField(max_digits=4, decimal_places=2)
+    num_nodes = models.IntegerField(null=True)
+    acc_50 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_60 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_70 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_80 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_90 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_95 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    acc_99 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
 
     def __str__(self):
         return "%s Prediction Model" % self.for_stock
