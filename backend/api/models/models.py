4e10f15ecd0e7ce3393b1746f89676e00aa2870e
Christian O'Neill <oneill.christian97@gmail.com>
2021-12-25 21:27:47 +0100

    Revert changes from previous migration
    Add minimum value validator to version check

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -1,18 +1,19 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class PredictionModel(models.Model):
     VERSIONS = []
 
-    version = models.CharField(max_length=1000, choices=VERSIONS, null=True, blank=True)
-    for_stock = models.CharField(max_length=10)
-    num_nodes = 0
-    acc_50 = 0
-    acc_60 = 0
-    acc_70 = 0
-    acc_80 = 0
-    acc_90 = 0
-    acc_95 = 0
-    acc_99 = 0
+    version = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    for_stock = models.CharField(max_length=200)
+    num_nodes = models.IntegerField()
+    acc_50 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_60 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_70 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_80 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_90 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_95 = models.DecimalField(max_digits=4, decimal_places=2)
+    acc_99 = models.DecimalField(max_digits=4, decimal_places=2)
 
     def __str__(self):
         return "%s Prediction Model" % self.for_stock
